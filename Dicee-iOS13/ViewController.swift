a03a26fa22b79bcff4d4d89437adc68774b62eff
journeyman32 <bryan@coderbox.co>
2022-12-13 21:59:41 -0600

    Modify controls in code

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix") // #imageLiteral
+        diceImageViewOne.alpha = 0.5
+        
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo") // #imageLiteral
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
 }
 
